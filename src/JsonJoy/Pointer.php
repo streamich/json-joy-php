2639db95117b19effbda59ca3bfe21bd94fb637c
streamich <streamich@gmail.com>
2020-12-21 12:22:31 +0100

    style: 💄 run linter

--- src/JsonJoy/Pointer.php
+++ src/JsonJoy/Pointer.php
@@ -3,17 +3,24 @@ namespace JsonJoy;
 
 use InvalidArgumentException;
 
-class Pointer {
-  static public function create(string $pointer): Pointer {
-    if (strlen($pointer) == 0) return new Pointer([]);
-    if ($pointer[0] != '/') throw new InvalidArgumentException('POINTER_INVALID');
-    $referenceTokens = explode('/', substr($pointer, 1));
-    return new Pointer($referenceTokens);
-  }
+class Pointer
+{
+    public static function create(string $pointer): Pointer
+    {
+        if (strlen($pointer) == 0) {
+            return new Pointer([]);
+        }
+        if ($pointer[0] != '/') {
+            throw new InvalidArgumentException('POINTER_INVALID');
+        }
+        $referenceTokens = explode('/', substr($pointer, 1));
+        return new Pointer($referenceTokens);
+    }
 
-  public array $referenceTokens;
+    public array $referenceTokens;
 
-  public function __construct(array $referenceTokens) {
-    $this->referenceTokens = $referenceTokens;
-  }
+    public function __construct(array $referenceTokens)
+    {
+        $this->referenceTokens = $referenceTokens;
+    }
 }
--- tests/unit/JsonJoy/PointerTest.php
+++ tests/unit/JsonJoy/PointerTest.php
@@ -2,22 +2,26 @@
 
 use PHPUnit\Framework\TestCase;
 
-final class PointerTest extends TestCase {
-  public function testCanParseARootPointer(): void {
-    $result = JsonJoy\Pointer::create('');
-    $expected = new JsonJoy\Pointer([]);
-    $this->assertEquals($expected, $result);
-  }
+final class PointerTest extends TestCase
+{
+    public function testCanParseARootPointer(): void
+    {
+        $result = JsonJoy\Pointer::create('');
+        $expected = new JsonJoy\Pointer([]);
+        $this->assertEquals($expected, $result);
+    }
 
-  public function testCanParseASimpleJsonPointer(): void {
-    $result = JsonJoy\Pointer::create('/foo/bar');
-    $expected = new JsonJoy\Pointer(['foo', 'bar']);
-    $this->assertEquals($expected, $result);
-  }
+    public function testCanParseASimpleJsonPointer(): void
+    {
+        $result = JsonJoy\Pointer::create('/foo/bar');
+        $expected = new JsonJoy\Pointer(['foo', 'bar']);
+        $this->assertEquals($expected, $result);
+    }
 
-  public function testThrowsOnInvalidPointer(): void {
-    $this->expectException(InvalidArgumentException::class);
-    $this->expectExceptionMessage('POINTER_INVALID');
-    $result = JsonJoy\Pointer::create('foo/bar');
-  }
+    public function testThrowsOnInvalidPointer(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('POINTER_INVALID');
+        $result = JsonJoy\Pointer::create('foo/bar');
+    }
 }
